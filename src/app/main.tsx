8d90b440b766510665669f110bceedc514cf8415
mickeyescaleto <mickeyescaleto@gmail.com>
2025-05-31 20:02:18 +0300

    feat: add jetbrains-mono font

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -1,6 +1,9 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
+import '@/app/styles/fonts.css';
+import '@/app/styles/main.css';
+
 createRoot(document.getElementById('application')!).render(
   <StrictMode>
     <h1>3D Visualization</h1>
